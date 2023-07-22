79bce8fa39cc830d0fa669289898fea54796d40a
Kamoliddin <kamoliddinxudoyberdiyev72@gmail.com>
2023-07-22 13:19:22 +0500

    created ws server

---
+++ server.js
@@ -0,0 +1,22 @@
+const WebSocket = require("ws");
+
+const server = new WebSocket.Server({ port: 3000 }, () => {
+    console.log(`Chat has started`);
+});
+
+server.on("connection", (ws) => {
+    console.log("New user connected");
+
+    ws.on("message", (message) => {
+        if (message == "/exit") {
+            ws.close();
+        } else {
+            server.clients.forEach((client) => {
+                if (client.readyState == WebSocket.OPEN) {
+                    client.send(message.toString());
+                }
+            });
+        }
+    });
+    ws.send("Welcome to chat 👋");
+})
